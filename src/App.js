4b9eef5c5f8cc6486e10aefc28fe846a1a6227f2
masandovva <mrianasndoval@hotmail.com>
2022-10-06 20:15:58 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+    </div>
+  );
+}
+
+export default App;
