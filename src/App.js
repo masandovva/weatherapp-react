01e439f2b937570643410e684aa96fc68774236d
masandovva <mrianasndoval@hotmail.com>
2022-10-06 20:42:39 -0700

    Get Github URL

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by <a href="https://www.linkedin.com/in/mariana-sandovala/" target= "_blank"> @Masandovva </a>and it is an {""}
+      <a href = "https://github.com/masandovva/weatherapp-react" target= "_blank">
+     open-sourced on Github
+      </a>
+      </footer>
     </div>
   );
 }
 
-export default App;
