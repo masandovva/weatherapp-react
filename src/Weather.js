5200043b2ebe6c9b453a7f51c0a3f6b966e62ed1
masandovva <mrianasndoval@hotmail.com>
2022-10-07 08:20:12 -0700

    HTML Pt.1

---
+++ src/Weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather(){
+    return <div className="Weather">
+     <h1> San Francisco</h1> 
+    <ul> 
+        <li> Wednesday 7:00</li> 
+        <li>Mostly Cloudy</li> 
+        </ul></div>
+    
+}
