3b5f98fbcd6de133e1f54a130d8e346f64799d45
masandovva <mrianasndoval@hotmail.com>
2022-10-06 21:21:07 -0700

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project is created by <a href="https://www.linkedin.com/in/mariana-sandovala/" target= "_blank"> @Masandovva </a>and it is an {""}
       <a href = "https://github.com/masandovva/weatherapp-react" target= "_blank">
      open-sourced on Github
       </a>
+      <button className="btn btn-ptimary" >Hello </button>
       </footer>
+      </div>
     </div>
   );
 }
